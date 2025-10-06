7499e66fa11832e0e08470d7816b926652b4e19a
Fraz Mahmud <fraz501@hotmail.com>
2025-10-06 19:01:07 -0400

    moved back c++ version to 23 and added some debug text

--- Pong/src/pong.cpp
+++ Pong/src/pong.cpp
@@ -8,7 +8,7 @@ Game breakout;
 double deltaTime = 0.0;
 uint64 lastFrame = 0;
 
-SDL_AppResult SDL_AppInit([[maybe_unused]]void **appstate, [[maybe_unused]]int argc, [[maybe_unused]]char *argv[]) {
+SDL_AppResult SDL_AppInit([[maybe_unused]] void **appstate, [[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     if (!breakout.Init(800, 600)) {
         SDL_Log("Could not initialize Game.");
         return SDL_APP_FAILURE;
@@ -17,13 +17,13 @@ SDL_AppResult SDL_AppInit([[maybe_unused]]void **appstate, [[maybe_unused]]int a
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent([[maybe_unused]]void *appstate, SDL_Event *event) {
+SDL_AppResult SDL_AppEvent([[maybe_unused]] void *appstate, SDL_Event *event) {
     const auto sdlAppState = breakout.ProcessInput(event);
 
     return sdlAppState;
 }
 
-SDL_AppResult SDL_AppIterate([[maybe_unused]]void *appstate) {
+SDL_AppResult SDL_AppIterate([[maybe_unused]] void *appstate) {
     // Update
     const uint64 currentFrame = SDL_GetPerformanceCounter();
     deltaTime = static_cast<double>(currentFrame - lastFrame) / static_cast<double>(SDL_GetPerformanceFrequency());
@@ -36,9 +36,12 @@ SDL_AppResult SDL_AppIterate([[maybe_unused]]void *appstate) {
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit([[maybe_unused]]void *appstate, SDL_AppResult result) {
+void SDL_AppQuit([[maybe_unused]] void *appstate, SDL_AppResult result) {
     SDL_Log("%s", "");
+#if defined(DEBUG) || defined(_DEBUG)
     if (result == SDL_APP_SUCCESS) SDL_Log("Exiting with status EXIT_SUCCESS");
     else SDL_Log("Exiting with status EXIT_FAILURE");
+#endif
+
     SDL_Quit();
 }
