db2f3e3214e0e735de71d869875f755a6fd6dd08
Fraz Mahmud <fraz501@hotmail.com>
2026-03-29 20:55:30 -0400

    added letter and pillar boxing, added a Game scene

---
+++ FlappyBirdUnity/Assets/Scripts/CameraLetterBox.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraLetterBox : MonoBehaviour
+{
+    [SerializeField] private int targetXAspect = 16;
+    [SerializeField] private int targetYAspect = 9;
+    private float _targetAspectRatio;
+
+    private Camera _camera;
+    private int _lastWidth;
+    private int _lastHeight;
+    private Rect _originalCameraRect;
+
+    private void Awake()
+    {
+        _targetAspectRatio = (float)targetXAspect / targetYAspect;
+        _camera = GetComponent<Camera>();
+        _originalCameraRect = _camera.rect;
+        UpdateCamera();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Screen.width != _lastWidth || Screen.height != _lastHeight)
+        {
+            UpdateCamera();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_camera is not null)
+        {
+            _camera.rect = _originalCameraRect;
+        }
+    }
+
+    private void UpdateCamera()
+    {
+        _lastWidth = Screen.width;
+        _lastHeight = Screen.height;
+
+        var windowAspectRatio = (float)Screen.width / Screen.height;
+        var scaleHeight = windowAspectRatio / _targetAspectRatio;
+
+        var rect = _camera.rect;
+
+        if (scaleHeight < 1.0f)
+        {
+            // Add letterbox (black bars top/bottom)
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+        }
+        else
+        {
+            // Add pillar-box (black bars left/right)
+            var scaleWidth = 1.0f / scaleHeight;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+        }
+
+        _camera.rect = rect;
+    }
+}
