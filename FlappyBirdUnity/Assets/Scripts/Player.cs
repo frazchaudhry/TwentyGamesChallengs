0d998531f6713a2f7c41b6ca1f625ef059f8bdaa
Fraz Mahmud <fraz501@hotmail.com>
2026-04-05 21:22:36 -0400

    replaced input to use Unity Input Actions

--- FlappyBirdUnity/Assets/Scripts/Player.cs
+++ FlappyBirdUnity/Assets/Scripts/Player.cs
@@ -7,17 +7,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float rotationSpeed = 50.0f;
     
     private  Rigidbody2D _rb;
+    private InputAction _flapAction;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _flapAction = InputSystem.actions.FindAction("Flap");
+        _flapAction.Enable();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (_flapAction.WasPressedThisFrame())
         {
             _rb.linearVelocity = Vector2.up * upwardImpulse;
             transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
