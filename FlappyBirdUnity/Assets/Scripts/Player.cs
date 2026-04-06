7e9236d07a89f834568be390f5cc86f568e824e3
Fraz Mahmud <fraz501@hotmail.com>
2026-04-05 23:44:45 -0400

    added a reference to input action flap instead of finding it by name

--- FlappyBirdUnity/Assets/Scripts/Player.cs
+++ FlappyBirdUnity/Assets/Scripts/Player.cs
@@ -5,22 +5,21 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float upwardImpulse = 1.0f;
     [SerializeField] private float rotationSpeed = 50.0f;
+    [SerializeField] private InputActionProperty flapAction;
     
     private  Rigidbody2D _rb;
-    private InputAction _flapAction;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _flapAction = InputSystem.actions.FindAction("Flap");
-        _flapAction.Enable();
+        flapAction.action.Enable();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (_flapAction.WasPressedThisFrame())
+        if (flapAction.action.WasPressedThisFrame())
         {
             _rb.linearVelocity = Vector2.up * upwardImpulse;
             transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
